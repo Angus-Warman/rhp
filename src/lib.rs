7770f925285ff6691a35814d7aab3bfade7c95ba
Angus-Warman <anguswarman@gmail.com>
2026-08-12 18:47:32 +0800

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use std::path::{Path, PathBuf};
 
 use axum::{
-    Router, extract::Request, http::StatusCode,
-    response::{Html, IntoResponse, Response}, routing::{any},
+    Router, extract::{Request, State}, http::StatusCode, response::{Html, IntoResponse, Response}, routing::any,
 };
 use tower::util::ServiceExt;
 use tower_http::services::ServeDir;
@@ -19,24 +18,34 @@ mod value;
 
 pub async fn run_server(port: u16, folder: PathBuf, _db_conn: &str) {
     let addr = format!("0.0.0.0:{port}");
-    let app = build_router();
+    let app = build_router(folder);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     dbg!(&listener);
     axum::serve(listener, app).await.unwrap();
 }
 
-fn build_router() -> Router {
+#[derive(Clone)]
+struct AppState {
+    folder: PathBuf,
+}
+
+fn build_router(folder: PathBuf) -> Router {
+    let state = AppState {
+        folder: folder,
+    };
+
     let app = Router::new()
         .route("/{*path}", any(rhp_handler))
+        .with_state(state)
         .layer(TraceLayer::new_for_http());
 
     app
 }
 
-async fn rhp_handler(request: Request) -> Response {
-    let path = request.uri().path();
+async fn rhp_handler(State(state): State<AppState>, request: Request) -> Response {
+    let path = state.folder.join(request.uri().path());
 
-    if Path::new(path).extension().map_or(false, |ext| ext == "rhp") {
+    if Path::new(&path).extension().map_or(false, |ext| ext == "rhp") {
         process_rhp(path).await.into_response()
     } else {
         ServeDir::new("public")
@@ -46,8 +55,7 @@ async fn rhp_handler(request: Request) -> Response {
     }
 }
 
-async fn process_rhp(path: &str) -> Response {
-    let path = "./public/".to_string() + path;
+async fn process_rhp(path: PathBuf) -> Response {
     match tokio::fs::read_to_string(path).await {
         Ok(src) => Html(process_src(&src)).into_response(),
         Err(_) => StatusCode::NOT_FOUND.into_response(),
--- src/lib_tests.rs
+++ src/lib_tests.rs
@@ -2,9 +2,13 @@ use super::*;
 
 use axum_test::TestServer;
 
+fn test_server() -> TestServer {
+    TestServer::new(build_router("./public".into()))
+}
+
 #[tokio::test]
 async fn test_hello_world() {
-    let server = TestServer::new(build_router());
+    let server = test_server();
     let response = server.get("/hello.rhp").await;
     response.assert_status_ok();
     response.assert_text_contains("Hello\nWorld");
@@ -12,7 +16,7 @@ async fn test_hello_world() {
 
 #[tokio::test]
 async fn test_non_rhp() {
-    let server = TestServer::new(build_router());
+    let server = test_server();
     let response = server.get("/plain.html").await;
     response.assert_status_ok();
     response.assert_text_contains("Plain");
